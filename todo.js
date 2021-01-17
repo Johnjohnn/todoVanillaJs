c0d0045d264b78c9fdf32463ac0a185e59c875dd
johnjohnn <conde2010@aol.com>
2021-01-17 00:06:30 -0500

    adding more functions

--- todo.js
+++ todo.js
@@ -1,5 +1,26 @@
-const hello = " hello world " 
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos")
 
-const greeting =() {
-     hello;
-};
+const generateTemplate = todo => {
+     const html =`
+     <li class="list-group-item d-flex justify-content-between align-items-center">
+     <span>${todo}</span>
+     <i class="far fa-trash-alt delete"></i>
+     </li>
+     `;
+     list.innerHTML += html;
+
+}
+
+
+
+addForm.addEventListener("submit", e =>{
+
+     e.preventDefault();                                                    // very important to prevent default value!!!
+    const todo =  addForm.add.value.trim();                                 // the trimp method removes the whie values
+   if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+   }
+  
+})
