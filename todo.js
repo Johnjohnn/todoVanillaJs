166fb3834f019bc191fea1026e7f4128afd131f8
johnjohnn <conde2010@aol.com>
2021-01-17 01:45:20 -0500

    adding a filter

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos")
+const search = document.querySelector(".search input")
 
 const generateTemplate = todo => {
      const html =`
@@ -23,4 +24,34 @@ addForm.addEventListener("submit", e =>{
         addForm.reset();
    }
   
+})
+
+
+//DELETE TODOS 
+list.addEventListener("click", e => {
+ if(e.target.classList.contains("delete")){
+      e.target.parentElement.remove()
+ }
+})
+const filterTodos = (term)=> {
+     Array.from(list.children)
+     .filter((todo) => !todo.textContent.includes(term))
+     .forEach((todo)=> todo.classList.add("filtered"));
+          
+     Array.from(list.children)
+     .filter((todo) => todo.textContent.includes(term))
+     .forEach((todo)=> todo.classList.remove("filtered"));
+             
+          
+          
+
+
+}
+
+
+
+// KEYUP EVENT 
+search.addEventListener("keyup", ()=>{
+     const term = search.value.trim();
+     filterTodos(term);
 })
